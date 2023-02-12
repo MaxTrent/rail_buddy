b9be50dee093daf02d0a609210acd1edfdd0ec05
Samuel Adelowo <samueladelowo92@gmail.com>
2023-02-12 11:34:34 +0100

    Sign In Page

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:train_app/trainAppTextFormField.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,15 +20,18 @@ class MyApp extends StatelessWidget {
     ]);
     return ScreenUtilInit(
       builder: (context, child)=> MaterialApp(
+        themeMode: ThemeMode.system,
+        debugShowCheckedModeBanner: false,
         title: 'Train Ticketing App',
         theme: ThemeData(
+          textTheme: GoogleFonts.nunitoTextTheme(Theme.of(context).textTheme),
           primarySwatch: Colors.blue,
         ),
         initialRoute: LoginScreen.id,
         routes: {
           LoginScreen.id: (context) => LoginScreen(),
         },
-        home: LoginScreen(),
+
       ),
     );
   }
@@ -34,6 +39,8 @@ class MyApp extends StatelessWidget {
 
 class LoginScreen extends StatefulWidget {
 static const String id = 'login-screen';
+
+
   @override
   State<LoginScreen> createState() => _LoginScreenState();
 }
@@ -44,49 +51,25 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(25.0),
-            child: TextFormField(
-              controller: usernameController,
-              decoration: InputDecoration(
-               // suffixIcon: suffixIcon,
-                contentPadding: const EdgeInsets.fromLTRB(16, 0, 0, 0),
-                //hintText: hintText,
-                fillColor: Theme.of(context).backgroundColor,
-                filled: false,
-                errorStyle: TextStyle(height: 0, color: Colors.transparent),
-                hintStyle: TextStyle(
-                  fontSize: 16,
-                  color: Color(0xFF969A9D),
-                  fontWeight: FontWeight.w300,
-                ),
-                border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10.r),
-                    borderSide: BorderSide(color: Color(0xFF707070), width: .5)),
-                enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.zero,
-                    borderSide: BorderSide(color: Color(0xFF707070), width: .5)),
-                focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.zero,
-                    borderSide: BorderSide(color: Color(0xFF707070), width: .5)),
-                errorBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.zero,
-                  borderSide: BorderSide(color: Colors.red, width: .5),
-                ),
-              ),
-        style: TextStyle(
-            fontSize: 16,
-            color: Color(0xFF3C3C43),
-
-              ),
-            ),
-          ),
-        ],
+      backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+      //appBar: AppBar(),
+      body: Padding(
+        padding: const EdgeInsets.all(25.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text('Sign In', style: TextStyle(
+              color: Colors.black,
+              fontSize: 33.sp,
+              fontWeight: FontWeight.w600,
+            ),),
+            SizedBox(height: 25.h,),
+            TrainTextFormField(controller: usernameController, hintText: 'Username',),
+            SizedBox(height: 30.h,),
+            TrainTextFormField(controller: usernameController, hintText: 'Password',),
+          ],
+        ),
       ),
     );
   }
--- lib/trainAppTextFormField.dart
+++ lib/trainAppTextFormField.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class TrainTextFormField extends StatefulWidget {
   final TextEditingController controller;
+  final String? hintText;
 
-  const TrainTextFormField({Key? key}) : super(key: key);
+  const TrainTextFormField({required this.controller, this.hintText});
 
   @override
   State<TrainTextFormField> createState() => _TrainTextFormFieldState();
@@ -13,31 +15,32 @@ class _TrainTextFormFieldState extends State<TrainTextFormField> {
   @override
   Widget build(BuildContext context) {
     return TextFormField(
-      controller: controller,
+      controller: widget.controller,
       decoration: InputDecoration(
         // suffixIcon: suffixIcon,
         contentPadding: const EdgeInsets.fromLTRB(16, 0, 0, 0),
         //hintText: hintText,
         fillColor: Theme.of(context).backgroundColor,
         filled: false,
-        errorStyle: TextStyle(height: 0, color: Colors.transparent),
+        hintText: widget.hintText,
+        errorStyle: TextStyle(height: 0, color: Colors.red),
         hintStyle: TextStyle(
           fontSize: 16,
           color: Color(0xFF969A9D),
           fontWeight: FontWeight.w300,
         ),
         border: OutlineInputBorder(
-            borderRadius: BorderRadius.zero,
-            borderSide: BorderSide(color: Color(0xFF707070), width: .5)),
+            borderRadius: BorderRadius.circular(10.r),
+            borderSide: BorderSide(color: Color(0xFF707070), width: 2)),
         enabledBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.zero,
-            borderSide: BorderSide(color: Color(0xFF707070), width: .5)),
+            borderRadius: BorderRadius.circular(10.r),
+            borderSide: BorderSide(color: Color(0xFF707070), width: 2)),
         focusedBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.zero,
-            borderSide: BorderSide(color: Color(0xFF707070), width: .5)),
+            borderRadius: BorderRadius.circular(10.r),
+            borderSide: BorderSide(color: Color(0xFF707070), width: 2)),
         errorBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.zero,
-          borderSide: BorderSide(color: Colors.red, width: .5),
+          borderRadius: BorderRadius.circular(10.r),
+          borderSide: BorderSide(color: Colors.red, width: 2),
         ),
       ),
       style: TextStyle(
