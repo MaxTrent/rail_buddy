0b8e47f54a31c425e9570d49fda7861b5b22a5ee
SteveTayor <ayomidearose75@gmail.com>
2023-02-13 17:25:47 +0100

    admin update profile page

--- lib/screens/admin/admin_update_profile_screen.dart
+++ lib/screens/admin/admin_update_profile_screen.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../../railBuddyTextFormField.dart';
 
 class AdminUpdateProfileScreen extends StatefulWidget {
+  static const String id = 'admin-update-profile';
   const AdminUpdateProfileScreen({Key? key}) : super(key: key);
 
   @override
@@ -9,10 +13,144 @@ class AdminUpdateProfileScreen extends StatefulWidget {
 }
 
 class _AdminUpdateProfileScreenState extends State<AdminUpdateProfileScreen> {
+  TextEditingController nameController = TextEditingController();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController phoneNumberController = TextEditingController();
+  TextEditingController cityController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Scaffold(),
+      child: Scaffold(
+        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+        appBar: AppBar(
+          toolbarHeight: 80,
+          backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+          elevation: 0.0,
+          title: Padding(
+            padding: const EdgeInsets.only(left: 8.0, top: 25),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: InkWell(
+                    onTap: () {},
+                    child: Text(
+                      'Cancel',
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: Colors.blue.shade600,
+                      ),
+                    ),
+                  ),
+                ),
+                const Text(
+                  'Update Profile',
+                  style: TextStyle(
+                    fontSize: 27,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: InkWell(
+                    onTap: () {},
+                    child: Text(
+                      'Save',
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: Colors.black26,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(
+                  left: 89,
+                ),
+                child: Container(
+                  height: 250,
+                  width: 190,
+                  child: Image.asset(
+                    'assets/images/avatar.png',
+                    fit: BoxFit.fill,
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 5.h,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 28.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text('Name',
+                          style:
+                              // GoogleFonts.nunito(
+                              TextStyle(
+                            fontSize: 16.sp,
+                            fontWeight: FontWeight.w600,
+                          )
+                          // ),
+                          ),
+                    ),
+                    SizedBox(
+                        height: 50.h,
+                        width: 300.w,
+                        child: RailBuddyTextFormField(
+                          obscure: true,
+                          controller: nameController,
+                          hintText: 'John Rice',
+                          borderSide: BorderSide.none,
+                          keyboardType: TextInputType.emailAddress,
+                        )),
+                    SizedBox(
+                      height: 5.h,
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text('Email',
+                              style:
+                                  // GoogleFonts.nunito(
+                                  TextStyle(
+                                fontSize: 16.sp,
+                                fontWeight: FontWeight.w600,
+                              )
+                              // ),
+                              ),
+                        ),
+                        SizedBox(
+                            height: 50.h,
+                            width: 300.w,
+                            child: RailBuddyTextFormField(
+                              controller: emailController,
+                              hintText: 'johnrice@gmail.com',
+                              borderSide: BorderSide.none,
+                              keyboardType: TextInputType.emailAddress,
+                            )),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
